305160b741a8fdfd4fecfcf066613158a59a6eab
Quiinn Chang <quiinn.personal@gmail.com>
2026-04-22 02:02:36 +0800

    fix CORS for Vercel

--- server/index.js
+++ server/index.js
@@ -89,7 +89,15 @@ async function initDB() {
   console.log('Database initialised')
 }
 
-app.use(cors())
+app.use(cors({
+  origin: [
+    'https://agency-hub-teal.vercel.app',
+    'http://localhost:3000',
+    'http://localhost:5173',
+    'http://192.168.0.7:3000'
+  ],
+  credentials: true
+}))
 app.use(express.json())
 
 app.get('/', function(req, res) {
